83e98a7b476230e0443ae7343c096b38beb15d36
Ben Mechen <benm123@yahoo.com>
2026-04-02 13:16:02 +0100

    Update $incidentId.dets.tsx

--- src/routes/incidents/$date/$incidentId.dets.tsx
+++ src/routes/incidents/$date/$incidentId.dets.tsx
@@ -158,7 +158,8 @@ function RouteComponent() {
               <IcAlert
                 variant="info"
                 heading="Did you know?"
-                message="This form is automatically saved to your browser as you type"
+                message="This form is automatically saved to your browser as you type. It can be exported to an email using the 'Export' button at the top of the page."
+                dismissible
               />              
               <IcSelect
                 placeholder="Select dets template..." 
