6a5cdd280223b7e014264874aa9c93e956854072
Ben Mechen <benm123@yahoo.com>
2026-04-02 09:19:12 +0100

    Update variable.tsx

--- src/components/statements/new/editor/variable.tsx
+++ src/components/statements/new/editor/variable.tsx
@@ -1,11 +1,1 @@
-import { NodeViewContent, NodeViewWrapper } from "@tiptap/react";
 
-export const Variable = () => {
-  return (
-    <NodeViewWrapper className="bg-blue-500 p-2 rounded-sm">
-      <label contentEditable={false}>React Component</label>
-
-      <NodeViewContent className="content is-editable" />
-    </NodeViewWrapper>
-  );
-};
