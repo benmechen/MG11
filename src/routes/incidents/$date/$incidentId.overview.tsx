e37988d2814d630d8e18ae5e02761d72fe281b94
Ben Mechen <benm123@yahoo.com>
2026-04-02 12:15:10 +0100

    Update $incidentId.overview.tsx

--- src/routes/incidents/$date/$incidentId.overview.tsx
+++ src/routes/incidents/$date/$incidentId.overview.tsx
@@ -161,6 +161,7 @@ function RouteComponent() {
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {people?.map((person, index) => (
+              
               <PersonCard key={person.id} person={person} index={index} />
             ))}
             <div
