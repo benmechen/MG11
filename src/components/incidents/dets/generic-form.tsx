94c12f1314191e18a0049ef490dec0b6e48dab1b
Ben Mechen <benm123@yahoo.com>
2026-04-02 13:07:07 +0100

    Update generic-form.tsx

--- src/components/incidents/dets/generic-form.tsx
+++ src/components/incidents/dets/generic-form.tsx
@@ -25,8 +25,7 @@ export const GenericForm = () => (
 - Take care to only record sensitive personal data on relevant person cards.
 - Ensure that all officers in attendance are recorded.
 - Establish details of all offences alleged.
-- What are the points to prove for the offence and are they complete?
-"
+- What are the points to prove for the offence and are they complete?"
         spellCheck
         autoCapitalize="on"
         fullWidth
@@ -34,5 +33,81 @@ export const GenericForm = () => (
         resize 
         /*{...register('generalActions')}*/
       />
+      <IcTextField
+        label="Scenes"
+        helperText="Specify actions taken to identify & preserve crime scenes, and to minimise identified risks and hazards"
+        value="- Specify actions taken to identify & preserve crime scenes, and to minimise identified risks and hazards.
+- Consider victims route into the scene - where had they been just before?
+- Specify positive and negative CCTV enquiries undertaken. (Consider Local Authority, Private CCTV & DashCams)
+- Has identified CCTV been viewed? If not, what type of system?"
+        spellCheck
+        autoCapitalize="on"
+        fullWidth
+        rows={10} 
+        resize 
+        /*{...register('generalActions')}*/
+      />
+      <IcTextField
+        label="Forensic"
+        helperText="Specify actions to preserve forensic evidence and prevent cross contamination"
+        value="- Specify actions to preserve forensic evidence and prevent cross contamination.
+- Consider whether Suspect touched Victim (think Blood, Saliva, trace DNA)
+- If evidence is seized, record Exhibit Number and location.
+- Was an Early Evidence Kit used?
+- Has SOCO been liaised with at scene? Include Forensic Examiner Jobsheet Number, if known."
+        spellCheck
+        autoCapitalize="on"
+        fullWidth
+        rows={10} 
+        resize 
+        /*{...register('generalActions')}*/
+      />
+      <IcTextField
+        label="Victims and Witnesses"
+        helperText="Outline the evidence each can provide and obtain statements"
+        value="- Outline the evidence each can provide and obtain statements.
+- Record appearance and clothing for elimination purposes. Identify significant witnesses.
+- Where needed, confirm medical consent obtained from the victim and document/photograph injuries.
+- Specify positive and negative Local Directed enquiries to locate further witnesses.
+- Have victim or witness MG11's been taken?"
+        spellCheck
+        autoCapitalize="on"
+        fullWidth
+        rows={10} 
+        resize 
+        /*{...register('generalActions')}*/
+      />
+      <IcTextField
+        label="Suspects"
+        value="- Ensure the First Description is recorded and circulated, including any words used and any distinctive elements.
+- Document any ID procedures conducted including Drive Round & Street ID / Video Capture in custody.
+- Document any arrest enquiries undertaken.
+- If suspect arrested - have s18/32 searches been completed?
+- Consider photographing / BWV of clothing worn at time of arrest.
+- On arrest, were checks completed for Tagging Spray / Smartwater etc."
+        spellCheck
+        autoCapitalize="on"
+        fullWidth
+        rows={10} 
+        resize 
+        /*{...register('generalActions')}*/
+      />
+      <IcTextField
+        label="Any other investigative details/actions"
+        value="- Such as: Details of any CID officer spoken to
+- Details of any Drive Round
+- Usage details of stolen cards;
+- Ensure victim obtains IMEI from provider or watch serial number
+- Details of any tracking software enabled/ activated;
+- Any reasons for any late reporting"
+        spellCheck
+        autoCapitalize="on"
+        fullWidth
+        rows={10} 
+        resize 
+        /*{...register('generalActions')}*/
+      />
+
+
   </div>
 );
